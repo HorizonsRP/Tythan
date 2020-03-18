1cb2ed50e5d47289126a6e7e326d3ab4ce2bdb8c
Matthew Hildebrand <korvic@live.ca>
2020-03-18 00:37:40 -0500

    Added permissions util page to store some utils there. Javadoc comments included.

---
+++ tythan-bukkit/src/main/java/co/lotc/core/bukkit/util/PermissionsUtil.java
@@ -0,0 +1,49 @@
+package co.lotc.core.bukkit.util;
+
+import net.luckperms.api.LuckPerms;
+import net.luckperms.api.model.user.User;
+import net.luckperms.api.node.Node;
+import org.bukkit.Bukkit;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.RegisteredServiceProvider;
+
+import java.util.UUID;
+
+public class PermissionsUtil {
+
+	/**
+	 * Gets the highest numerical representation for a given permission node. permission.*, *, and permission.unlimited grant max value.
+	 * @param player UUID of the player in question.
+	 * @param permission Start of the permission node to check. ex: tythan.permission will look for the highest x in tythan.permission.x
+	 * @return Highest permission node value.
+	 */
+	public static int getMaxPermission(UUID player, String permission, int defaultAmount) {
+		int output = defaultAmount;
+		RegisteredServiceProvider<LuckPerms> provider = Bukkit.getServicesManager().getRegistration(LuckPerms.class);
+		if (provider != null) {
+			LuckPerms api = provider.getProvider();
+			User user = api.getUserManager().getUser(player);
+			if (user != null) {
+				for (Node node : user.getNodes()) {
+					if (node.getValue()) {
+						String thisPerm = node.getKey();
+						if (thisPerm.equalsIgnoreCase(permission + ".unlimited") ||
+							thisPerm.equalsIgnoreCase(permission + ".*") ||
+							thisPerm.equalsIgnoreCase("*")) {
+							output = Integer.MAX_VALUE;
+							break;
+						} else if (thisPerm.startsWith(permission)) {
+							String[] split = node.getKey().replace('.', ' ').split(" ");
+							int value = Integer.parseInt(split[split.length-1]);
+							if (value > output) {
+								output = value;
+							}
+						}
+					}
+				}
+			}
+		}
+		return output;
+	}
+
+}
