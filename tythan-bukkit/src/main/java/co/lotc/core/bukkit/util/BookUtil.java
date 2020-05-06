cf7b5f8c36fc83e41bc28910ddde16b6effe3c58
Matthew Hildebrand <korvic@live.ca>
2020-05-05 21:58:14 -0500

    Add page parser within Tythan to aid with BookStream.

---
+++ tythan-bukkit/src/main/java/co/lotc/core/bukkit/util/BookUtil.java
@@ -0,0 +1,38 @@
+package co.lotc.core.bukkit.util;
+
+import org.bukkit.ChatColor;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.BookMeta;
+
+public class BookUtil {
+
+	public String getPagesAsString(ItemStack book) {
+		if (book.getItemMeta() instanceof BookMeta) {
+			return getPagesAsString((BookMeta) book.getItemMeta());
+		}
+		return null;
+	}
+
+	public String getPagesAsString(BookMeta meta) {
+		StringBuilder combinedDesc = new StringBuilder();
+		for (String str : meta.getPages()) {
+			if (combinedDesc.length() > 0) {
+				combinedDesc.append(" ");
+			}
+			if (str.contains("\n")) {
+				String[] lineBreaks = str.split("\\R", -1);
+				for (int i = 0; i < lineBreaks.length; i++) {
+					combinedDesc.append(ChatColor.stripColor(lineBreaks[i]));
+					if (i < lineBreaks.length-1) {
+						combinedDesc.append(" \n ");
+					}
+				}
+			} else {
+				combinedDesc.append(ChatColor.stripColor(str));
+			}
+		}
+
+		return combinedDesc.toString();
+	}
+
+}
