9b1e0e760842d471702f83880f1df18e4c34a1f6
Matthew Hildebrand <korvic@live.ca>
2020-06-10 12:08:22 -0500

    Update all of PermissionUtil to use Atomic method of going through user completable futures.

--- tythan-bukkit/src/main/java/co/lotc/core/bukkit/util/PermissionsUtil.java
+++ tythan-bukkit/src/main/java/co/lotc/core/bukkit/util/PermissionsUtil.java
@@ -16,56 +16,67 @@ public class PermissionsUtil {
 
 	/**
 	 * Gets the highest numerical representation for a given permission node. permission.*, *, and permission.unlimited grant max value.
+	 * @param base An AtomicInteger that is used as the default number and the integer to check when complete.
 	 * @param player UUID of the player in question.
 	 * @param permission Start of the permission node to check. ex: tythan.permission will look for the highest x in tythan.permission.x
-	 * @return Highest permission node value.
+	 * @return An AtomicBoolean that will update to true when the check is complete.
 	 */
-	public static int getMaxPermission(UUID player, String permission, int defaultAmount) {
-		return getMinMaxPermission(player, permission, defaultAmount, false);
+	public static AtomicBoolean getMaxPermission(AtomicInteger base, UUID player, String permission) {
+		return getMinMaxPermission(base, player, permission, false);
 	}
 
 	/**
 	 * Gets the lowest numerical representation for a given permission node. permission.*, *, and permission.unlimited grant max value.
+	 * @param base An AtomicInteger that is used as the default number and the integer to check when complete.
 	 * @param player UUID of the player in question.
 	 * @param permission Start of the permission node to check. ex: tythan.permission will look for the highest x in tythan.permission.x
-	 * @return Lowest permission node value.
+	 * @return An AtomicBoolean that will update to true when the check is complete.
 	 */
-	public static int getMinPermission(UUID player, String permission, int defaultAmount) {
-		return getMinMaxPermission(player, permission, defaultAmount, true);
+	public static AtomicBoolean getMinPermission(AtomicInteger base, UUID player, String permission) {
+		return getMinMaxPermission(base, player, permission, true);
 	}
 
-	private static int getMinMaxPermission(UUID player, String permission, int defaultAmount, boolean min) {
-		int output = defaultAmount;
+	private static AtomicBoolean getMinMaxPermission(AtomicInteger base, UUID player, String permission, boolean min) {
+		AtomicBoolean finished = new AtomicBoolean(false);
 		RegisteredServiceProvider<LuckPerms> provider = Bukkit.getServicesManager().getRegistration(LuckPerms.class);
 		if (provider != null) {
 			LuckPerms api = provider.getProvider();
 			CompletableFuture<User> future = api.getUserManager().loadUser(player);
-			User user = future.join();
-			if (user != null) {
-				for (Node node : user.getDistinctNodes()) {
-					String key = node.getKey();
-					if (key.startsWith(permission)) {
-						String[] split = key.split("\\.");
-						try {
-							int value = Integer.parseInt(split[split.length - 1]);
-							if (min) {
-								if (value < output) {
-									output = value;
+			future.thenAccept(user -> {
+				if (user != null) {
+					for (Node node : user.data().toCollection()) {
+						String key = node.getKey();
+						if (key.startsWith(permission)) {
+							String[] split = key.split("\\.");
+							try {
+								String data = split[split.length - 1];
+								if (data.equalsIgnoreCase("*") || data.equalsIgnoreCase("unlimited")) {
+									base.set(Integer.MAX_VALUE);
+									break;
+								} else {
+									int value = Integer.parseInt(data);
+									if (min) {
+										if (value < base.get()) {
+											base.set(value);
+										}
+									} else {
+										if (value > base.get()) {
+											base.set(value);
+										}
+									}
+									base.addAndGet(value);
 								}
-							} else {
-								if (value > output) {
-									output = value;
-								}
+							} catch (Exception e) {
+								TythanBukkit.get().getLogger().warning("Node failed to parse for min/max permission: " + key);
+								e.printStackTrace();
 							}
-						} catch (Exception e) {
-							TythanBukkit.get().getLogger().warning("Node failed to parse for total permission: " + key);
-							e.printStackTrace();
 						}
 					}
 				}
-			}
+				finished.set(true);
+			});
 		}
-		return output;
+		return finished;
 	}
 
 	public static AtomicBoolean getTotalPermission(AtomicInteger base, UUID player, String permission) {
@@ -81,8 +92,14 @@ public class PermissionsUtil {
 						if (key.startsWith(permission)) {
 							String[] split = key.split("\\.");
 							try {
-								int value = Integer.parseInt(split[split.length - 1]);
-								base.addAndGet(value);
+								String data = split[split.length - 1];
+								if (data.equalsIgnoreCase("*") || data.equalsIgnoreCase("unlimited")) {
+									base.set(Integer.MAX_VALUE);
+									break;
+								} else {
+									int value = Integer.parseInt(data);
+									base.addAndGet(value);
+								}
 							} catch (Exception e) {
 								TythanBukkit.get().getLogger().warning("Node failed to parse for total permission: " + key);
 								e.printStackTrace();
