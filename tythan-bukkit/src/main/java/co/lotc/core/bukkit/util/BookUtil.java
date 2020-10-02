fcaa9dcddbdac013070a433ced1f9bc5cc6a0cdc
Matthew Hildebrand <korvic@live.ca>
2020-10-01 21:27:28 -0500

    Add a useful util for books with common formatting methods.

--- tythan-bukkit/src/main/java/co/lotc/core/bukkit/util/BookUtil.java
+++ tythan-bukkit/src/main/java/co/lotc/core/bukkit/util/BookUtil.java
@@ -3,7 +3,9 @@ package co.lotc.core.bukkit.util;
 import org.bukkit.ChatColor;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.BookMeta;
+import org.bukkit.inventory.meta.ItemMeta;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class BookUtil {
@@ -97,6 +99,34 @@ public class BookUtil {
 			}
 		}
 		return combinedDesc.toString();
+	}
+
+	/**
+	 * @param string Returns a book-friendly page-list with the contents
+	 *               as the given string. Replaces a few combos with
+	 *               unique codes.
+	 *               [*1] = Small 1x1 Dot
+	 *               [*]  = Medium 2x2 Dot
+	 *               [*4] = Large 4x4 Dot
+	 *               [**] = Split Diamond
+	 */
+	public static List<String> getPagesForString(String string) {
+		List<String> output = new ArrayList<>();
+		string = string.replace("･", "[*1]");
+		string = string.replace("•", "[*]");
+		string = string.replace("●", "[*4]");
+		string = string.replace("❖", "[**]");
+		int pages = (int) Math.ceil(string.length() / 255d); // 255 is the max chars per page in current MC books.
+		for (int i = 1; i <= pages; i++) {
+			int j = (i - 1) * 255;
+			int k = i * 255;
+
+			if (string.length() < k) {
+				k = string.length();
+			}
+			output.add(string.substring(j, k));
+		}
+		return output;
 	}
 
 }
